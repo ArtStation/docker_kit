ba736215d477b0fb27442b05252ef72496144d23
Iskander Khaziev <gvalmon@gmail.com>
2020-09-25 13:11:43 -0400

    added example image

--- lib/indocker/cli.rb
+++ lib/indocker/cli.rb
@@ -3,12 +3,15 @@ require 'cli/ui'
 
 class Indocker::CLI < Thor
 
-  desc "build IMAGE_NAME", "Build image with IMAGE_NAME"
-  def build(image_name)
+  desc "compile IMAGE_NAME", "Compile image with IMAGE_NAME"
+  def compile(image_name)
     CLI::UI::StdoutRouter.enable
     spin_group = ::CLI::UI::SpinGroup.new
     spin_group.add("Building #{image_name}") { |spinner| sleep 3.0; }
     spin_group.wait
+
+    #compiler = Indocker::Container['compiler.image_compiler']
+    #compiler.compile()
   end
 
   def self.exit_on_failure?
