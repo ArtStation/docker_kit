6dd4340703839bb862343e3c5c7b0265d5b3e86e
Iskander Khaziev <gvalmon@gmail.com>
2020-09-30 20:12:21 +0000

    added contract for image attributes

--- lib/indocker/core/image.rb
+++ lib/indocker/core/image.rb
@@ -1,6 +1,17 @@
 class Indocker::Core::Image
   attr_reader :name, :dependencies, :registry, :dockerfile_path, :build_args, :build_context_dir, :tag, :before_build_callback, :after_build_callback
 
+  Contract KeywordArgs[
+    name:               Symbol,
+    dependencies:       ArrayOf[Symbol],
+    registry:           Maybe[Indocker::Infrastructure::Registry],
+    dockerfile_path:    String,
+    build_args:         Hash,
+    build_context_dir:  Maybe[String],
+    tag:                String,
+    before_build_callback: Maybe[Proc],
+    after_build_callback:  Maybe[Proc]
+  ] => Any
   def initialize(name:, dependencies:, registry:, dockerfile_path:, build_args:, build_context_dir:, tag:, before_build_callback:, after_build_callback:)
     @name = name
     @dependencies = dependencies
