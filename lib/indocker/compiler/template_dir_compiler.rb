c41e84bce55f36cf7281daefe02e5fa2e3ff7e3d
Iskander Khaziev <gvalmon@gmail.com>
2020-10-07 11:50:35 -0400

    template dir compiler should copy files one by one instead of re-creating directory

--- lib/indocker/compiler/template_dir_compiler.rb
+++ lib/indocker/compiler/template_dir_compiler.rb
@@ -5,19 +5,15 @@ class Indocker::Compiler::TemplateDirCompiler
   ]
 
   def compile(shell, source_dir, destination_dir, context_helper: nil)
-    copy_dir(shell, source_dir, destination_dir)
-    compile_templates(shell, destination_dir, context_helper: context_helper)
+    shell.recursive_list_files(source_dir).each do |source_file_path|
+      relative_path = source_file_path.sub(source_dir, '')
+      destination_file_path = File.join(destination_dir, relative_path)
+
+      template_file_compiler.compile(
+        shell, source_file_path, 
+        destination_path: destination_file_path, 
+        context_helper: context_helper
+      )
+    end
   end
-
-  private
-    def copy_dir(shell, source_dir, destination_dir)
-      bash_commands.rm_rf(shell, destination_dir)
-      bash_commands.cp_r(shell, source_dir, destination_dir)
-    end
-
-    def compile_templates(shell, templates_dir, context_helper:)
-      shell.recursive_list_files(templates_dir).each do |file_path|
-        template_file_compiler.compile(shell, file_path, context_helper: context_helper)
-      end
-    end
 end
--- spec/indocker/compiler/template_dir_compiler_spec.rb
+++ spec/indocker/compiler/template_dir_compiler_spec.rb
@@ -11,26 +11,21 @@ RSpec.describe Indocker::Compiler::TemplateDirCompiler do
     FileUtils.rm_r(destination_dir) if File.exists?(destination_dir)
   end
 
-  it "re-creates compile dir" do
-    expect(subject.bash_commands).to receive(:rm_rf).with(shell, destination_dir)
-    expect(subject.bash_commands).to receive(:cp_r).with(shell, source_dir, destination_dir)
-
-    allow(subject).to receive(:compile_templates)
-
-    subject.compile(shell, source_dir, destination_dir, context_helper: context_helper)
-  end
-
   it "compiles all files in directory" do
     expect(shell).to receive(:recursive_list_files).and_return([
-      File.join(destination_dir, "erb_template.txt"),
-      File.join(destination_dir, "test.txt")
+      File.join(source_dir, "erb_template.txt"),
+      File.join(source_dir, "config/test.cfg")
     ])
 
     expect(subject.template_file_compiler).to receive(:compile).with(
-      shell, File.join(destination_dir, "erb_template.txt"), context_helper: context_helper
+      shell, File.join(source_dir, "erb_template.txt"), 
+      destination_path: File.join(destination_dir, "erb_template.txt"),
+      context_helper: context_helper
     )
     expect(subject.template_file_compiler).to receive(:compile).with(
-      shell, File.join(destination_dir, "test.txt"), context_helper: context_helper
+      shell, File.join(source_dir, "config/test.cfg"),
+      destination_path: File.join(destination_dir, "config/test.cfg"),
+      context_helper: context_helper
     )
 
     subject.compile(shell, source_dir, destination_dir, context_helper: context_helper)
