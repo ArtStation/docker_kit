51c0cf9a57fd9ea309c11c71233b781a349e0cf2
Iskander Khaziev <gvalmon@gmail.com>
2021-02-24 23:48:33 +0000

    remove test kubeconfig file

--- example/configurations/review.rb
+++ example/configurations/review.rb
@@ -2,6 +2,5 @@ KuberKit
   .define_configuration(:review)
   .use_registry(:review_default, as: :default)
   .use_artifact(:kuber_kit_repo, as: :kuber_kit_repo)
-  .kubeconfig_path("/home/iskander/data/artstation_api/kuber_kit/clusters/prints_api_rke.yml")
   #.deployer_require_confirimation
   #.use_build_server(:remote_bs)
