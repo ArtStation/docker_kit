df0cabfd148c51860e121677b26064587b3986a2
Iskander Khaziev <gvalmon@gmail.com>
2024-04-13 18:54:18 +0000

    added more examples for the docker app

--- example/services/docker_app.rb
+++ example/services/docker_app.rb
@@ -2,12 +2,19 @@ KuberKit
   .define_service(:docker_app)
   .images(:ruby_app)
   .deployer_strategy(:docker)
-  .attributes(
-    deployer: {
-      detached:         false,
-      image_name:       :ruby_app,
-      container_name:   "test_docker_app",
-      delete_if_exists: true,
-      env_file_names:   [:test]
+  .attributes do
+    artifact_store = KuberKit::Core::Artifacts::ArtifactStore.new
+    artifact  = artifact_store.get(:kuber_kit_repo)
+    root_path = artifact.cloned_path
+
+    {
+      deployer: {
+        detached:         false,
+        container_name:   "test_docker_app",
+        image_name:       :ruby_app,
+        networks:         ["test_network"],
+        volumes:          ["#{root_path}:/app"],
+        delete_if_exists: true
+      }
     }
-  )
+  end
